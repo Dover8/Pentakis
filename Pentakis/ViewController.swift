d520dc60bbeaf1c2eee03909233de9193fa2a4fa
Ben MacKinnon <ben.mackinnon@soluis.com>
2020-02-12 14:06:39 +0000

    Drawing the pentikis to screen

--- Pentakis/ViewController.swift
+++ Pentakis/ViewController.swift
@@ -8,9 +8,21 @@
 
 import GLKit
 
+extension Array {
+    func size() -> Int {
+        return MemoryLayout<Element>.stride * self.count
+    }
+}
+
 class ViewController: GLKViewController {
 
     private var context: EAGLContext?
+    private var pentikisIndiceBuffer = GLuint()
+    private var pentikisPointBuffer = GLuint()
+    private var vbo = GLuint()
+    private var vao = GLuint()
+    private var effect = GLKBaseEffect()
+    private var rotation: Float = 0.0
     
     private func setupGL() {
         context = EAGLContext(api: .openGLES3)
@@ -21,6 +33,70 @@ class ViewController: GLKViewController {
             view.context = context
             delegate = self
         }
+        
+        let vertexAttribColor = GLuint(GLKVertexAttrib.color.rawValue)
+        let vertexAttribPosition = GLuint(GLKVertexAttrib.position.rawValue)
+        let vertexSize = MemoryLayout<ColoredVertex>.stride
+        let colorOffset = MemoryLayout<GLfloat>.stride * 3
+        let colofOffsetPointer = UnsafeRawPointer(bitPattern: colorOffset)
+        
+        glGenVertexArraysOES(1, &vao)
+        
+        glBindVertexArrayOES(vao)
+        
+        glGenBuffers(1, &vbo)
+        glBindBuffer(GLenum(GL_ARRAY_BUFFER), vbo)
+        glBufferData(GLenum(GL_ARRAY_BUFFER),
+                     Vertices.size(),
+                     Vertices,
+                     GLenum(GL_STATIC_DRAW))
+        
+        glEnableVertexAttribArray(vertexAttribPosition)
+        glVertexAttribPointer(vertexAttribPosition, 3,
+                              GLenum(GL_FLOAT),
+                              GLboolean(UInt8(GL_FALSE)),
+                              GLsizei(vertexSize),
+                              nil)
+        
+        glEnableVertexAttribArray(vertexAttribColor)
+        glVertexAttribPointer(vertexAttribColor,
+                              4,
+                              GLenum(GL_FLOAT),
+                              GLboolean(UInt8(GL_FALSE)),
+                              GLsizei(vertexSize),
+                              colofOffsetPointer)
+        
+        glGenBuffers(1, &pentikisIndiceBuffer)
+        glBindBuffer(GLenum(GL_ELEMENT_ARRAY_BUFFER), pentikisIndiceBuffer)
+        glBufferData(GLenum(GL_ELEMENT_ARRAY_BUFFER),
+                     Pairs.size(),
+                     Pairs,
+                     GLenum(GL_STATIC_DRAW))
+        
+//        glGenBuffers(1, &pentikisPointBuffer)
+//        glBindBuffer(GLenum(GL_ELEMENT_ARRAY_BUFFER), pentikisPointBuffer)
+//        glBufferData(GLenum(GL_ELEMENT_ARRAY_BUFFER),
+//                     Points.size(),
+//                     Points,
+//                     GLenum(GL_STATIC_DRAW))
+        
+        // clear the buffers
+        glBindVertexArray(0)
+        glBindBuffer(GLenum(GL_ARRAY_BUFFER), 0)
+        glBindBuffer(GLenum(GL_ELEMENT_ARRAY_BUFFER), 0)
+    }
+    
+    private func tearDownGL() {
+        EAGLContext.setCurrent(context)
+        
+        glDeleteBuffers(1, &vao)
+        glDeleteBuffers(1, &vbo)
+        glDeleteBuffers(1, &pentikisIndiceBuffer)
+        glDeleteBuffers(1, &pentikisPointBuffer)
+        
+        EAGLContext.setCurrent(nil)
+        
+        context = nil
     }
     
     override func viewDidLoad() {
@@ -32,12 +108,37 @@ class ViewController: GLKViewController {
         glClearColor(0.85, 0.85, 0.85, 10)
         
         glClear(GLbitfield(GL_COLOR_BUFFER_BIT))
+        
+        effect.prepareToDraw()
+        glBindVertexArray(vao)
+        glDrawElements(GLenum(GL_LINES),
+                       GLsizei(Pairs.count),
+                       GLenum(GL_UNSIGNED_BYTE),
+                       nil)
+        glBindVertexArray(0)
+    }
+    
+    deinit {
+        tearDownGL()
+    }
+    
     }
 
 }
 
 extension ViewController: GLKViewControllerDelegate {
     func  glkViewControllerUpdate(_ controller: GLKViewController) {
+        let aspect = fabsf(Float(view.bounds.size.width) / Float(view.bounds.size.height))
         
+        let projectionMatrix = GLKMatrix4MakePerspective(GLKMathDegreesToRadians(65.0), aspect, 0.1, 50.0)
+        
+        effect.transform.projectionMatrix = projectionMatrix
+        
+        var modelViewMatrix = GLKMatrix4MakeTranslation(0, 0, -7.5)
+        
+        //rotation += 90 * Float(timeSinceLastUpdate)
+        //modelViewMatrix = GLKMatrix4Rotate(modelViewMatrix, GLKMathDegreesToRadians(rotation), 1, 1, 1)
+        
+        effect.transform.modelviewMatrix = modelViewMatrix
     }
 }
