cd92afe4aa7b24218f13707a2552f9aacb499b67
Ben MacKinnon <ben.mackinnon@soluis.com>
2020-02-12 10:05:01 +0000

    Set up base GLKit app view

---
+++ Pentakis/Vertex.swift
@@ -0,0 +1,10 @@
+//
+//  Vertex.swift
+//  Pentakis
+//
+//  Created by Ben MacKinnon on 12/02/2020.
+//  Copyright © 2020 Dover. All rights reserved.
+//
+
+import GLKit
+
--- Pentakis/ViewController.swift
+++ Pentakis/ViewController.swift
@@ -6,15 +6,38 @@
 //  Copyright © 2020 Dover. All rights reserved.
 //
 
-import UIKit
+import GLKit
 
-class ViewController: UIViewController {
+class ViewController: GLKViewController {
 
+    private var context: EAGLContext?
+    
+    private func setupGL() {
+        context = EAGLContext(api: .openGLES3)
+        
+        EAGLContext.setCurrent(context)
+        
+        if let view = self.view as? GLKView, let context = context {
+            view.context = context
+            delegate = self
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupGL()
     }
-
+    
+    override func glkView(_ view: GLKView, drawIn rect: CGRect) {
+        glClearColor(0.85, 0.85, 0.85, 10)
+        
+        glClear(GLbitfield(GL_COLOR_BUFFER_BIT))
+    }
 
 }
 
+extension ViewController: GLKViewControllerDelegate {
+    func  glkViewControllerUpdate(_ controller: GLKViewController) {
+        
+    }
+}
